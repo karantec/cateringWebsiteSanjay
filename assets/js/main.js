c859493ddf48c1d04d1517311637010fd643de6f
Yogesh <yogeshgiri6806@gmail.com>
2023-10-02 01:46:12 +0530

    Fixed non-response of active links in the navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -228,6 +228,20 @@
   });
 
   /**
+   * Making active button in navbar responsive
+   */
+  var navDiv = document.getElementById("navbar");
+  var links = navDiv.getElementsByClassName("nav-link");
+
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+
+  /**
    * Testimonials slider
    */
   new Swiper('.testimonials-slider', {
